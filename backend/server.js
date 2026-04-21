e4477c2e924724793c2b2e8239e6dd494f0341c6
Luis Jale <pruebagmail.com>
2026-04-21 18:24:07 -0400

    Actualizar MONGO_URI version

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,15 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('../frontend'));
-
 const transacciones = require('./routes/transacciones');
 app.use('/api/transacciones', transacciones);
 
-    mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
+
 .then(() => console.log('Conectado a MongoDB'))
 .catch((err) => console.error('Error al conectar a MongoDB:', err));
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
-    });
+});
+
